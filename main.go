71b366fa8ac809fed0073037f307f6c9418cdc3c
echenim <myron.echenim@gmail.com>
2020-10-01 23:14:28 -0700

    set up for containerizartion

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"os/exec"
 
+	"github.com/echenim/microcloud/utils"
+)
+
+// container entry point
+func main() {
+	switch os.Args[1] {
+	case "run":
+		run()
+	default:
+		panic("what??")
+
+	}
+}
+
+func run() {
+	fmt.Printf("running %v\n", os.Args[2:])
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	utils.Check(cmd.Run(), "FAILED")
 }
