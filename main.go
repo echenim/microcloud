b16c858480a9d9fbc101d6ef2261df9d97b3aff5
echenim <myron.echenim@gmail.com>
2020-10-06 02:14:36 -0700

    first commit

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+//26028f81c5ec522c80b21d909a2333b6357cf74f
+
 import (
 	"fmt"
 	"os"
@@ -14,7 +16,7 @@ func main() {
 	case "run":
 		run()
 	default:
-		panic("what??")
+		panic("what?S???")
 
 	}
 }
